5fc16c0e501ca20ec36e5cd9cdbfb8dc30f4ad47
Aryan salge <aryansalge02@gmail.com>
2022-09-19 23:08:01 +0530

    Add files via upload

---
+++ jseg/script.js
@@ -0,0 +1,9 @@
+document.getElementById("ans").innerHTML = "A PIECE OF SHIT";
+document.getElementById("ans").style.color='white';
+document.getElementById('ary').style.fontSize='40px';
+document.getElementById("ary").style.color='pink';
+
+document.body.style.backgroundColor="green";
+
+
+
